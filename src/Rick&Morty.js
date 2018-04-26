97041ed7f228d75cad09520e11df9e570b67c653
Adrian Messado and Jared Lutz <jared@>
2018-04-26 11:45:14 -0700

    delete empty spaces

--- src/Rick&Morty.js
+++ src/Rick&Morty.js
@@ -1,7 +1,3 @@
-
-
-
-
 function indexCheck(){
   let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let stringSplit = string.split("");
